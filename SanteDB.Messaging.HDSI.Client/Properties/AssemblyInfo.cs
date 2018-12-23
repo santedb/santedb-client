9e9f0f0691cee61709055e7cb22b593426ed9750
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-12-23 16:26:29 +0630

    revised version codes

--- SanteDB.Messaging.HDSI.Client/Properties/AssemblyInfo.cs
+++ SanteDB.Messaging.HDSI.Client/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.10.0.*")][assembly: AssemblyInformationalVersion("Fredericton")]
-[assembly: AssemblyVersion("1.10.0.*")][assembly: AssemblyInformationalVersion("Fredericton")]
+// [assembly: AssemblyVersion("1.11.0.*")][assembly: AssemblyInformationalVersion("Gananoque")]
+[assembly: AssemblyVersion("1.11.0.*")][assembly: AssemblyInformationalVersion("Gananoque")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
